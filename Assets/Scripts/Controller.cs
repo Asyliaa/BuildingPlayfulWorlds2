862913c77eed84a55584bb8f208c65d82ece02c3
Asyliaa <Asyliaa@users.noreply.github.com>
2018-06-24 13:49:32 +0200

    second commit
    beetje bezig proberen te zijn met errors fixen

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -40,7 +40,10 @@ public class Controller : MonoBehaviour
     public Text tempText;
     public Text enemyText;
 
-    public bool attackBool; 
+    public bool attackBool;
+    public bool plantBool;
+    public bool groundBool;
+    public bool caudronBool;
     public GameObject enemy;
     private int damage = 10; 
     public Text healthText;
@@ -49,7 +52,7 @@ public class Controller : MonoBehaviour
     //andere script van de timer importeren zodat ik die kan gebruiken om de eindscore weer te geven
     public TimerScript timerScript;
 
-    public int playerHealth = 0;
+    private int playerHealth;
     
 
     void Start()
@@ -67,11 +70,11 @@ public class Controller : MonoBehaviour
         scoreText.text = "";
         gieterText.text = "";
         collectedText.text = "Collected:";
-        neededText.text = "Needed: 1 seed, 1 flower";
-        healthText.text = "100";
+        neededText.text = "Needed: 1 seed, 1 flower, 1 crystal";
+        healthText.text = "Health = 100";
         tempText.text = "";
         playerHealth = 100;
-        enemyText.text = "test";
+        enemyText.text = "Enemy Health";
 
 
     }
@@ -84,14 +87,56 @@ public class Controller : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.E))
                 {
                     enemy.GetComponent<EnemyScript>().enemyHealth -= damage;
-                    enemyText.text = "Enemy took damage";
+                    enemyText.text = "Enemy health =" + enemy.GetComponent<EnemyScript>().enemyHealth.ToString();
+                gieterText.text = "Enemy took 10 damage!";
                     print("It workds");
                 }
 
             }
             
 
-        
+        if (plantBool == true)
+        {
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                collectedText.text = "Collected: 2 Seeds";
+                neededText.text = "Needed: 1 flower, 1 crystal";
+                gieterText.text = "Obtained 2 seeds";
+                print("it works!");
+                objectCount = objectCount + 1; 
+                
+            }
+
+        }
+
+        if (groundBool == true)
+        {
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                gieterText.text = "Planted 1 seed";
+                collectedText.text = "Collected: 1 Seed";
+                neededText.text = "Needed: 1 flower, 1 crystal";
+                print("it wooorks!");
+                objectCount = objectCount + 1;
+            }
+        }
+
+        if (caudronBool == true)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                winText.text = "YOU DID IT!";
+                gieterText.text = "";
+                collectedText.text = "";
+                neededText.text = "";
+                //op het eind timer laten zien
+
+                scoreText.text = "Your score is: " + timerScript.counterText.text;
+            }
+        }
+
 
         //Get input from WASD
         verticalAxis = Input.GetAxis("Vertical");
@@ -147,6 +192,140 @@ public class Controller : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
 
+   
+        //als je een pick up object oppakt dan gaat het object weg en telt je score op.
+        //Room 1 
+        if (other.gameObject.CompareTag("Pick Up Gieter"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            objectCount = objectCount + 1;
+            gieterText.text = "Obtained watering can";
+
+            //object count is 1 
+        }
+        
+        if (other.gameObject.CompareTag("Plant"))
+        {         
+            if (objectCount >= 1)
+            {
+                tempText.text = "Water me and I'll give you two seeds! (Press F to water)";
+                plantBool = true;
+                }
+
+            if (objectCount >= 2)
+            {
+                plantBool = false;
+                tempText.text = "";
+            }
+
+            if (objectCount < 1)
+            {
+                tempText.text = "Water me and I'll give you two seeds! (You need a watering can.)";
+            }
+               
+
+        }
+
+
+        if (other.gameObject.CompareTag("Schep"))
+        {
+           
+                other.gameObject.SetActive(false);
+                objectCount = objectCount + 1;
+                gieterText.text = "Obtained shovel";
+
+                //object count is 3
+                    }
+
+        if (other.gameObject.CompareTag("Grond"))
+        {
+            if (objectCount >= 3)
+            {
+                groundBool = true;
+                tempText.text = "Press F to plant seed";
+               
+            }
+
+            if (objectCount >= 4)
+            {
+                groundBool = false; 
+            }
+            if (objectCount < 3)
+            {
+                tempText.text = "Needed to plant a seed: seed, watering can, shovel.";
+            }
+
+            if (triggerCount >= 1)
+            {
+                tempText.text = "";
+                collectedText.text = "Collected: 1 Seed, 1 Flower";
+                neededText.text = "Needed: 1 crystal";
+                gieterText.text = "Obtained 1 flower";
+            }
+        }
+
+        if (other.gameObject.CompareTag("Grown"))
+        {
+            gieterText.text = "Your plant has fully grown! Return to collect it.";
+            triggerCount = triggerCount + 1;
+            other.gameObject.SetActive(false);
+        }
+
+
+
+        //Room 2
+
+        if (other.gameObject.CompareTag("Feed"))
+        {
+            tempText.text = "I'm hungry. In return for green food I will give you this crystal I found.";
+            if (objectCount >= 5)
+            {
+                tempText.text = "Thank you! Have my crystal";
+                collectedText.text = "Collected: 1 Seed, 1 Flower, 1 Crystal.";
+                neededText.text = "";
+                gieterText.text = "You collected all the ingredients! Find the cauldron and put them in there to finish the potion.";
+
+            }
+        }
+
+        if (other.gameObject.CompareTag("Green Food"))
+        {
+            other.gameObject.SetActive(false);
+
+            gieterText.text = "Obtained: Green Food";
+            objectCount = objectCount + 1;
+
+            //object count is 5
+        }
+
+        //Room 3
+
+        if (other.gameObject.CompareTag("Key"))
+        {
+            other.gameObject.SetActive(false);
+            objectCount = objectCount + 1;
+            gieterText.text = "Obtained: Key";
+
+            
+            // object count is 6
+        }
+
+        if (other.gameObject.CompareTag("Door"))
+        {
+            if (objectCount >= 6)
+            {
+                other.gameObject.SetActive(false);
+            }
+        }
+
+        //als je in de trigger voor informatie loopt, roept hij die functie aan en krijg je informatie te zien. 
+        if (other.gameObject.CompareTag("Info"))
+        {
+            SetInfoText();
+        }
+
+        //room 4
         //Boolean voor enemy zodat je in Update damage kan doen. Hier zorg ik ervoor dat hij true wordt. 
         if (other.gameObject.CompareTag("Enemy"))
         {
@@ -159,127 +338,24 @@ public class Controller : MonoBehaviour
         {
 
             playerHealth = playerHealth - 10;
-            healthText.text = "90";
+            healthText.text = "Health = " + playerHealth.ToString();
 
             if (playerHealth <= 0)
             {
                 gieterText.text = "YOU DIED";
             }
         }
-        //als je een pick up object oppakt dan gaat het object weg en telt je score op.
-        //Room 1 
-        if (other.gameObject.CompareTag("Pick Up Gieter"))
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-            objectCount = objectCount + 1;
-            gieterText.text = "Obtained: Gieter!";
 
-            //object count is 1 
-        }
-
-        if (other.gameObject.CompareTag("Plant"))
+        if (other.gameObject.CompareTag("Cauldron"))
         {
-            if (objectCount >= 1)
+            if (objectCount >= 6)
             {
+                caudronBool = true;
+                tempText.text = "Press F to put in all the ingredients!";
 
-
-                collectedText.text = "Collected: 2 Seeds";
-                neededText.text = "Needed: 1 flower";
             }
 
         }
-
-
-        if (other.gameObject.CompareTag("Schep"))
-        {
-            if (objectCount >= 1)
-            {
-                other.gameObject.SetActive(false);
-                objectCount = objectCount + 1;
-                gieterText.text = "Obtained: Schep!";
-
-                //object count is 2
-            }
-
-        }
-
-        if (other.gameObject.CompareTag("Grond"))
-        {
-            if (objectCount >= 2)
-            {
-                gieterText.text = "Planted: 1 Seed!";
-                collectedText.text = "Collected: 1 Seed";
-            }
-
-            if (triggerCount >= 1)
-            {
-                collectedText.text = "Collected: 1 Seed, 1 Flower";
-                neededText.text = "Needed:";
-                gieterText.text = "";
-            }
-        }
-
-
-
-        //Room 2
-
-        if (other.gameObject.CompareTag("Feed"))
-        {
-            tempText.text = "I'm hungry. In return for green food I will give you this crystal I found.";
-            if (objectCount >= 3)
-            {
-                tempText.text = "Thank you! Have my crystal";
-                collectedText.text = "Collected: 1 Seed, 1 Flower, 1 Crystal.";
-
-            }
-        }
-
-        if (other.gameObject.CompareTag("Green Food"))
-        {
-            other.gameObject.SetActive(false);
-
-            gieterText.text = "Obtained: Green Food";
-            objectCount = objectCount + 1;
-
-            //object count is 3
-        }
-
-        //Room 3
-
-        if (other.gameObject.CompareTag("Key"))
-        {
-            other.gameObject.SetActive(false);
-            objectCount = objectCount + 1;
-            gieterText.text = "Obtained: Key";
-
-            //op het eind timer laten zien
-
-            scoreText.text = "Your score is: " + timerScript.counterText.text;
-            // object count is 4
-        }
-
-        if (other.gameObject.CompareTag("Door"))
-        {
-            if (objectCount >= 4)
-            {
-                other.gameObject.SetActive(false);
-            }
-        }
-
-        //als je in de trigger voor informatie loopt, roept hij die functie aan en krijg je informatie te zien. 
-        if (other.gameObject.CompareTag("Info"))
-        {
-            SetInfoText();
-        }
-
-        if (other.gameObject.CompareTag("Grown"))
-        {
-            gieterText.text = "Your plant has fully grown!";
-            triggerCount = triggerCount + 1;
-            other.gameObject.SetActive(false);
-        }
-
 
     }
  
@@ -303,14 +379,38 @@ public class Controller : MonoBehaviour
         {
             tempText.text = "";
         }
+
+        if (other.gameObject.CompareTag("Plant"))
+        {
+        
+                tempText.text = "";
+            plantBool = false;
+        }
+
+        if (other.gameObject.CompareTag("Grond"))
+        {
+            tempText.text = "";
+            groundBool = false; 
+        }
     }
 
 
     void SetInfoText()
     {
-        //de informatie text die je krijgt als je de info trigger inloopt.
-        infoText.text = "Hi there, you are a witch but you're losing you're not feeling well. Make a potion to feel better! Collect the following things: ";
-        controlText.text = "Use wasd to move and your mouse to look around. You can jump by pressing the spacebar and double jump by pressing it twice. ";
+
+        if ( objectCount < 3)
+        {
+            //de informatie text die je krijgt als je de info trigger inloopt.
+            infoText.text = "Hi there, you are a witch but you're losing you're not feeling well. Make a potion to feel better! Collect the following things: ";
+            controlText.text = "Use wasd to move and your mouse to look around. You can jump by pressing the spacebar and double jump by pressing it twice. ";
+        }
+
+        if (objectCount > 3)
+        {
+            infoText.text = "Watch out! There's and enemy ahead!";
+            controlText.text = "Avoid his magic balls and attack by pressing E.";
+        }
+     
     }
 
     void SetInfoDelText()
